037718be56976ac688440d46cd83f55af588683f
Uriel Engelberg & Lucas Tjor <uelt@localhost>
2025-03-30 16:22:41 -0300

    Agregar backups básicos a localStorage

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ const tareasUl = document.getElementById("tareasUl");
 let tareas = [];
 let ultimoTid;
 
+
+function cargarBackup() {
+    let doNew = false;
+    try {
+        tareas = JSON.parse(localStorage.getItem("tareas"));
+        ultimoTid = parseInt(localStorage.getItem("ultimoTid"));
+        doNew = tareas === null || isNaN(ultimoTid);
+    } catch (e) {
+        console.log("No se puede cargar un backup. Creando una nueva lista.");
+        doNew = true;
+    } finally {
+        if (doNew) {
+            tareas = [];
+            ultimoTid = 0;
+        }
+    }
+}
+function guardarBackup() {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    localStorage.setItem("ultimoTid", ultimoTid);
+}
+
+
 function agregarTareaLi(tarea) {
     const nuevoElemento = document.createElement("li");
     nuevoElemento.innerHTML = `${tarea.nombre}`;
@@ -16,7 +39,6 @@ function agregarTarea() {
 
     if (nuevaTareaInput.value !== "") {
         ultimoTid++;
-
         tarea = {
             tid: ultimoTid,
             fechaCreacion: new Date(),
@@ -26,6 +48,8 @@ function agregarTarea() {
 
         tareas.push(tarea);
         agregarTareaLi(tarea);
+        guardarBackup();
+
         nuevaTareaInput.value = "";
     }
 }
@@ -41,7 +65,11 @@ function eliminarTarea(pos) {
 function vaciar() {
     tareas = [];
     tareasUl.innerHTML = "";
+    localStorage.removeItem("tareas");
 }
+
+
+cargarBackup();
 
 nuevaTareaForm.addEventListener("submit", evento => {
     evento.preventDefault();
